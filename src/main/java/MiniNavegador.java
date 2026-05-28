f21abc447e11204ae962cd3cef057cabd85fd8f0
RatazanaEXE <claudia.r.carvalho2015@gmail.com>
2026-05-28 11:50:21 -0300

    primeira versão do Navegador

---
+++ src/main/java/MiniNavegador.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class MiniNavegador extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage palco) {
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+
+        TextField site = new TextField();
+        site.setPromptText("Digite o site aqui");
+
+
+        Button buscarSite = new Button("Buscar site");
+
+        buscarSite.setOnAction(e -> {
+            //String texto = site.getText();
+
+            webEngine.load(site.getText());
+        });
+
+        VBox layout = new VBox(10, site, buscarSite, webView);
+        Scene cena = new Scene(layout, 1200,700);
+        palco.setTitle("Navegado web");
+        palco.setScene(cena);
+        palco.show();
+    }
+}
